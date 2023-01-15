9fd64e5e046ad9ac20b9a84cf8c3207ac15928ea
Vitor Müller <vitormuller013@gmail.com>
2023-01-14 22:14:44 -0300

    skills ready and scrolls functions added

---
+++ src/components/layout/CardTools.jsx
@@ -0,0 +1,25 @@
+import "./CardTools.css"
+
+function CardTools(){
+    return (
+        <div className="card-tools">
+            <h3>Frontend developer</h3>
+            <div className="itens">
+                <ul>
+                    <li><i class='bx bxl-html5 icons' ></i>HTML</li>
+                    <li><i class='bx bxl-css3 icons' ></i>CSS</li>
+                    <li><i class='bx bxl-javascript icons' ></i>JavaScript</li>
+                </ul>
+                <ul>
+                    <li><i class='bx bxl-bootstrap icons' ></i>Bootstrap</li>
+                    <li><i class='bx bxl-git icons' ></i>Git</li>
+                    <li><i class='bx bxl-react icons' ></i>React</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default CardTools;
+
+// PHP LARAVEL APACHE MYSQL
