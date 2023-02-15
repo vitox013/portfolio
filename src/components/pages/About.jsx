61c47dabdaa181308ec3e6b6178f38968532bcd9
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2023-02-15 02:15:22 -0300

    update bio and cv

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,36 +1,61 @@
-import './About.css'
-import Container from '../layout/Container';
-import photo from "../../imgs/picture.png"
-import Intro from '../layout/Intro';
-import CardAbt from '../cards/CardAbt';
+import "./About.css";
+import Container from "../layout/Container";
+import photo from "../../imgs/picture.png";
+import Intro from "../layout/Intro";
+import CardAbt from "../cards/CardAbt";
 
-
-function About(){
+function About() {
     return (
         <Container customClass="aboutContainer">
-            <section className='about' id='sobre'>
-                <Intro intro="Sobre Mim" text="Minha apresentação"/>
-                <div className='desc_photo'>
-                    <div className='photo'>
+            <section className="about" id="sobre">
+                <Intro intro="Sobre Mim" text="Minha apresentação" />
+                <div className="desc_photo">
+                    <div className="photo">
                         <img src={photo} alt="" />
                     </div>
-                    <div className='desc'>
-                        <div className='icon'>
-                            <CardAbt icon="bx bxs-component" text="Organização" anima="bx-tada"/>
-                            <CardAbt icon="bx bx-play-circle" text="Hands On" anima="bx-flashing"/>
-                            <CardAbt icon="bx bx-analyse" text="Adaptável" anima="bx-spin"/>
+                    <div className="desc">
+                        <div className="icon">
+                            <CardAbt
+                                icon="bx bxs-component"
+                                text="Organização"
+                                anima="bx-tada"
+                            />
+                            <CardAbt
+                                icon="bx bx-play-circle"
+                                text="Hands On"
+                                anima="bx-flashing"
+                            />
+                            <CardAbt
+                                icon="bx bx-analyse"
+                                text="Adaptável"
+                                anima="bx-spin"
+                            />
                         </div>
-                        <div className='text'>
-                            <p>Comecei primeiramente pela área de hardware e no ano passado entrei no mundo da programação. </p>
-                            <p>Acredito que todo conhecimento que eu adquirir me ajudará no meio acadêmico e profissional.</p>
-                            <a href="https://drive.google.com/file/d/1hisPgrt6VKhtIBx6UbnRxs9omJLehg-D/view?usp=share_link" target="_blank"><button className='btn'> Ver Currículo <i class='bx bxs-file curr' ></i></button></a>
+                        <div className="text">
+                            <p>
+                                Comecei primeiramente pela área de hardware e
+                                após ter contato com linguagem Python em 2020, a
+                                história mudou.
+                            </p>
+                            <p>
+                                Atualmente focado em desenvolvimento Full-Stack.
+                            </p>
+                            <a
+                                href="https://drive.google.com/file/d/1iEpu130FJbzAPP9DlVrnsYMun0tcZVxc/view?usp=share_link"
+                                target="_blank"
+                            >
+                                <button className="btn">
+                                    {" "}
+                                    Ver Currículo{" "}
+                                    <i class="bx bxs-file curr"></i>
+                                </button>
+                            </a>
                         </div>
                     </div>
                 </div>
             </section>
         </Container>
-    )
-
+    );
 }
 
 export default About;
