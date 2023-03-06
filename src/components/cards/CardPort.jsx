5ac9b6896f7e7cb97939db7ca72ecb375195629c
Vitor Müller <85710199+vitox013@users.noreply.github.com>
2023-03-06 00:26:53 -0300

    added cardPort

--- src/components/cards/CardPort.jsx
+++ src/components/cards/CardPort.jsx
@@ -1,17 +1,20 @@
 import "./CardPort.css";
 
 function CardPort({ item }) {
-  return (
-    <div className={`cardPort`}>
-      <a href={item.url} target="_blank" key={item.id}>
-        <img src={item.image} alt="" />
-        <p className="title">{item.title}</p>
-        <p className="demo">
-          Demo <i class="bx bx-link-external seta"></i>
-        </p>
-      </a>
-    </div>
-  );
+    return (
+        <div className={`cardPort`}>
+            <a href={item.url} target="_blank" key={item.id}>
+                <img src={item.image} alt="" />
+                <p className="title">{item.title}</p>
+                <p className="demo">
+                    Demo <i class="bx bx-link-external seta"></i>
+                </p>
+            </a>
+            <a href={item.repository} target="_blank">
+                <i className="bx bxl-github git"></i>
+            </a>
+        </div>
+    );
 }
 
 export default CardPort;
--- src/components/data/PortfolioData.jsx
+++ src/components/data/PortfolioData.jsx
@@ -1,31 +1,42 @@
-import Work1 from "../../imgs/botlol.png";
-import Work2 from "../../imgs/curitiba.png";
+import Work2 from "../../imgs/botlol.png";
+import Work3 from "../../imgs/curitiba.png";
+import Work1 from "../../imgs/sellanne.png";
 
 export const projectsData = [
-  {
-    id: 1,
-    image: Work1,
-    url : "https://github.com/vitox013/bot-lol-api",
-    title: "Bot Lol",
-    category: "app",
-  },
-  {
-    id: 2,
-    image: Work2,
-    url : "https://vitox013.github.io/curitiba.tour/",
-    title: "Curitiba",
-    category: "web",
-  },
+    {
+        id: 1,
+        image: Work1,
+        url: "https://sellanne.onrender.com/",
+        title: "SellAnne",
+        category: "web",
+        repository: "https://github.com/vitox013/SellAnne",
+    },
+    {
+        id: 2,
+        image: Work2,
+        url: "https://github.com/vitox013/bot-lol-api",
+        title: "Bot Lol",
+        category: "app",
+        repository: "https://github.com/vitox013/bot-lol-api",
+    },
+    {
+        id: 3,
+        image: Work3,
+        url: "https://vitox013.github.io/curitiba.tour/",
+        title: "Curitiba",
+        category: "web",
+        repository: "https://github.com/vitox013/curitiba.tour",
+    },
 ];
 
 export const projectsNav = [
-  {
-    name: "all",
-  },
-  {
-    name: "web",
-  },
-  {
-    name: "app"
-  }
+    {
+        name: "all",
+    },
+    {
+        name: "web",
+    },
+    {
+        name: "app",
+    },
 ];
